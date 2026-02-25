5c4b3d2ca1a5619e9daa39ecd9efa472d4aa4cac
Josué Tille <josue@tille.ch>
2026-02-25 19:52:58 +0100

    Rework find-unref stae group script
    - Factorize script
    - Fix bug to make the build working
    - Avoid to compress the binary as it's quite small

--- build_unref.sh
+++ build_unref.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Note this script is made to be run on x86-64 architecture. Using it on other architecture will need some adaptations.
+
 # Enable set to be sure that all command don't fail
 set -eu
 
@@ -10,204 +12,76 @@ path_to_build="/opt/yunohost/$dir_name"
 #################################################################
 
 # commit of the sources to be built
-app_version="e873f9a"
-#app_version="$1"
-# architecture to be built
-arch_built="amd64"
-#result_suffix_name="$2"
-#result_prefix_name="$2"
+# app_version="e873f9a"
+app_version="$1"
 
 if [[ ! "$@" =~ "--chroot-yes" ]]
 then
-	echo "Est vous bien dans un chroot ? [y/n]"
-	read a
-	if [[ $a != "y" ]]
-	then
-		echo "Il est fortement conseillé d'être dans un chroot pour faire ces opérations"
-		exit 0
-	fi
+    echo "Est vous bien dans un chroot ? [y/n]"
+    read a
+    if [[ $a != "y" ]]
+    then
+        echo "Il est fortement conseillé d'être dans un chroot pour faire ces opérations"
+        exit 0
+    fi
 fi
 
 # Mount proc if it'isnt mouned.
 if [[ $(mount) != *"proc on /proc type proc"* ]]
 then
-	mount -t proc proc /proc
+    mount -t proc proc /proc
 fi
 
 # Upgrade system
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y cargo # rustc build-essential curl pkg-config
+apt-get install -y cargo gcc gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu
 
-cp $HOME/.cargo/config.toml /opt/yunohost/cargo_config_bkp.toml
-
-
-
-# Build amd64
-arch_built="amd64"
+for arch_built in amd64 arm64 armhf; do
+    dpkg --add-architecture ${arch_built}
+    apt-get install -y libstd-rust-dev:${arch_built} libc6-dev:${arch_built}
+done
 
 # Clean environnement
 rm -rf $path_to_build
-# Clean cargo build env
-rm -rf $HOME/.cargo/env
 
-echo "Start build time : $(date)" >> build_synapse_find_unref_stat_time.log
-
-# Install rustup
-if [ -z $(which rustup) ]; then
-    #curl -sSf -L https://static.rust-lang.org/rustup.sh | sh -s -- -y --default-toolchain=stable --profile=minimal
-    curl https://sh.rustup.rs -sSf | sh
-else
-    rustup update
-fi
-echo "[target.${arch_built}gc-unknown-linux-gnu]                                                                                       
-linker = '${arch_built}-linux-gnu-gcc'
-rustflags = ['-L', '/usr/lib/rustlib/${arch_built}gc-unknown-linux-gnu/lib']" >> $HOME/.cargo/config.toml
-
-source $HOME/.cargo/env
-#source ~/.bashrc 
-
-# Create new environnement
-#mkdir -p $path_to_build
-
-# Go in virtualenv
-old_pwd="${PWD/%\//}"
-
-git clone https://github.com/erikjohnston/synapse-find-unreferenced-state-groups.git $path_to_build
-pushd $path_to_build
-git reset --hard $app_version
-
-sudo dpkg --add-architecture ${arch_built}
-sudo apt update
-sudo apt install libstd-rust-dev:${arch_built} libc6-dev:${arch_built}
-cargo build --target ${arch_built}gc-unknown-linux-gnu
-cp /opt/yunohost/cargo_config_bkp.toml $HOME/.cargo/config.toml 
-
-cd ..
-
-# Build archive of binary and put everything on correct path to be used by auto update script
-tar -czf "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" "$dir_name"
-sha256sumarchive=$(sha256sum "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" | cut -d' ' -f1)
-mv "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" "$old_pwd"/
-echo $sha256sumarchive > "$old_pwd/${dir_name}_${app_version}-bin1_$arch_built-sha256.txt"
-
-popd
-
-echo "Finish build time : $(date)" >> build_synapse_find_unref_stat_time.log
-echo "sha256 SUM : $sha256sumarchive"
-
-
-# Build arm64
-arch_built="arm64"
-
-# Clean environnement
-rm -rf $path_to_build
-# Clean cargo build env
-rm -rf $HOME/.cargo/env
-
-echo "Start build time : $(date)" >> build_synapse_find_unref_stat_time.log
+export CARGO_HOME="$path_to_build/cargo"
+mkdir -p "$CARGO_HOME"
 
 # Install rustup
-if [ -z $(which rustup) ]; then
-    #curl -sSf -L https://static.rust-lang.org/rustup.sh | sh -s -- -y --default-toolchain=stable --profile=minimal
-    curl https://sh.rustup.rs -sSf | sh
-else
-    rustup update
-fi
-echo "[target.${arch_built}gc-unknown-linux-gnu]                                                                                       
-linker = '${arch_built}-linux-gnu-gcc'
-rustflags = ['-L', '/usr/lib/rustlib/${arch_built}gc-unknown-linux-gnu/lib']" >> $HOME/.cargo/config.toml
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable --profile=minimal
+$CARGO_HOME/bin/rustup target add aarch64-unknown-linux-gnu
+$CARGO_HOME/bin/rustup target add armv7-unknown-linux-gnueabihf
 
-source $HOME/.cargo/env
-#source ~/.bashrc 
-
-# Create new environnement
-#mkdir -p $path_to_build
-
-# Go in virtualenv
-old_pwd="${PWD/%\//}"
-
-git clone https://github.com/erikjohnston/synapse-find-unreferenced-state-groups.git $path_to_build
-pushd $path_to_build
-git reset --hard $app_version
-
-sudo dpkg --add-architecture ${arch_built}
-sudo apt update
-sudo apt install libstd-rust-dev:${arch_built} libc6-dev:${arch_built}
-cargo build --target ${arch_built}gc-unknown-linux-gnu
-cp /opt/yunohost/cargo_config_bkp.toml $HOME/.cargo/config.toml 
-
-cd ..
-
-# Build archive of binary and put everything on correct path to be used by auto update script
-tar -czf "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" "$dir_name"
-sha256sumarchive=$(sha256sum "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" | cut -d' ' -f1)
-mv "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" "$old_pwd"/
-echo $sha256sumarchive > "$old_pwd/${dir_name}_${app_version}-bin1_$arch_built-sha256.txt"
-
+git clone https://github.com/erikjohnston/synapse-find-unreferenced-state-groups.git "$path_to_build/repos"
+pushd "$path_to_build/repos"
+git reset --hard "$app_version"
 popd
 
-echo "Finish build time : $(date)" >> build_synapse_find_unref_stat_time.log
-echo "sha256 SUM : $sha256sumarchive"
+echo "[target.x86_64-unknown-linux-gnu]
+linker = 'x86_64-linux-gnu-gcc'
+rustflags = ['-L', '/usr/lib/rustlib/x86_64-unknown-linux-gnu/lib']
 
+[target.aarch64-unknown-linux-gnu]
+linker = 'aarch64-linux-gnu-gcc'
+rustflags = ['-L', '/usr/lib/rustlib/aarch64-unknown-linux-gnu/lib']
 
+[target.armv7-unknown-linux-gnueabihf]
+linker = 'arm-linux-gnueabihf-gcc'
+rustflags = ['-L', '/usr/lib/rustlib/armv7-unknown-linux-gnueabihf/lib']" >> "$CARGO_HOME/config.toml"
 
-# Build armhf
-arch_built="armhf"
+source "$CARGO_HOME/env"
 
-# Clean environnement
-rm -rf $path_to_build
-# Clean cargo build env
-rm -rf $HOME/.cargo/env
-
-echo "Start build time : $(date)" >> build_synapse_find_unref_stat_time.log
-
-# Install rustup
-if [ -z $(which rustup) ]; then
-    #curl -sSf -L https://static.rust-lang.org/rustup.sh | sh -s -- -y --default-toolchain=stable --profile=minimal
-    curl https://sh.rustup.rs -sSf | sh
-else
-    rustup update
-fi
-echo "[target.${arch_built}gc-unknown-linux-gnu]                                                                                       
-linker = '${arch_built}-linux-gnu-gcc'
-rustflags = ['-L', '/usr/lib/rustlib/${arch_built}gc-unknown-linux-gnu/lib']" >> $HOME/.cargo/config.toml
-
-source $HOME/.cargo/env
-#source ~/.bashrc 
-
-# Create new environnement
-#mkdir -p $path_to_build
-
-# Go in virtualenv
 old_pwd="${PWD/%\//}"
 
-git clone https://github.com/erikjohnston/synapse-find-unreferenced-state-groups.git $path_to_build
-pushd $path_to_build
-git reset --hard $app_version
+for arch_built in x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu armv7-unknown-linux-gnueabihf; do
+    pushd "$path_to_build/repos"
+    cargo build --release --target ${arch_built}
+    popd
+done
 
-sudo dpkg --add-architecture ${arch_built}
-sudo apt update
-sudo apt install libstd-rust-dev:${arch_built} libc6-dev:${arch_built}
-cargo build --target ${arch_built}gc-unknown-linux-gnu
-cp /opt/yunohost/cargo_config_bkp.toml $HOME/.cargo/config.toml 
-
-cd ..
-
-# Build archive of binary and put everything on correct path to be used by auto update script
-tar -czf "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" "$dir_name"
-sha256sumarchive=$(sha256sum "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" | cut -d' ' -f1)
-mv "${dir_name}_${app_version}-bin1_$arch_built.tar.gz" "$old_pwd"/
-echo $sha256sumarchive > "$old_pwd/${dir_name}_${app_version}-bin1_$arch_built-sha256.txt"
-
-popd
-
-echo "Finish build time : $(date)" >> build_synapse_find_unref_stat_time.log
-echo "sha256 SUM : $sha256sumarchive"
-
-rm -rf /opt/yunohost/cargo_config_bkp.toml
+cp "$path_to_build/repos/target/x86_64-unknown-linux-gnu/release/rust-synapse-find-unreferenced-state-groups" "rust-synapse-find-unreferenced-state-groups_amd64"
+cp "$path_to_build/repos/target/aarch64-unknown-linux-gnu/release/rust-synapse-find-unreferenced-state-groups" "rust-synapse-find-unreferenced-state-groups_arm64"
+cp "$path_to_build/repos/target/armv7-unknown-linux-gnueabihf/release/rust-synapse-find-unreferenced-state-groups" "rust-synapse-find-unreferenced-state-groups_armv7"
 
 exit 0
-
-
-
